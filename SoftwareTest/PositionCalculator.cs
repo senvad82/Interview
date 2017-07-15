7e5e2046f373033ba930819ce53f1e446f10b6ea
senthilvadivel venkatesan <senvad@gmail.com>
2017-07-15 16:19:25 -0400

    Initial Checkin

---
+++ SoftwareTest/PositionCalculator.cs
@@ -0,0 +1,67 @@
+﻿// This test should take around 45 minutes to 1 hour. Read all of the instructions before you being. It will help you plan ahead.
+
+/* TODO: Implement a net positions calculator
+ * This NetPositionCalculator should read in test_data.csv
+ * and output a file in the format of net_positions_expected.csv
+ * 
+ * Here is a sample of net positions:
+ * TRADER   BROKER  SYMBOL  QUANTITY    PRICE
+ * Joe      ML      IBM.N     100         50
+ * Joe      DB      IBM.N    -50          50
+ * Joe      CS      IBM.N     30          30
+ * Mike     CS      AAPL.N    100         20
+ * Mike     BC      AAPL.N    200         20
+ * Debby    BC      NVDA.N    500         20
+ * 
+ * Expected Output:
+ * TRADER   SYMBOL  QUANTITY
+ * Joe      IBM.N     80
+ * Mike     AAPL.N    300
+ * Debby    NVDA.N    500
+ */
+
+/* TODO: Implement a boxed position calculator
+ * This BoxedPositionCalculator should read in test_data.csv
+ * and output a file the format of boxed_positions_expected.csv
+ * 
+ * Boxed positions are defined as:
+ * A trader has long (quantity > 0) and short (quantity < 0) positions for the same symbol at different brokers.
+ * 
+ * This is an example of a boxed position:
+ * TRADER   BROKER  SYMBOL  QUANTITY    PRICE
+ * Joe      ML      IBM.N     100         50      <------Has at least one positive quantity for Trader = Joe and Symbol = IBM
+ * Joe      DB      IBM.N    -50          50      <------Has at least one negative quantity for Trader = Joe and Symbol = IBM
+ * Joe      CS      IBM.N     30          30
+ * 
+ * Expected Output:
+ * TRADER   SYMBOL  QUANTITY
+ * Joe      IBM.N     50        <------Show the minimum quantity of all long positions or the absolute sum of all short positions. ie. minimum of (100 + 30) and abs(-50) is 50
+ * 
+ * This is NOT a boxed position. Since no trader has both long and short positions at different brokers.
+ * TRADER   BROKER  SYMBOL  QUANTITY    PRICE
+ * Joe      ML      IBM.N     100         50
+ * Joe      DB      IBM.N     50          50
+ * Joe      CS      IBM.N     30          30
+ * Mike     DB      IBM.N    -50          50
+ * 
+ */
+
+/* TODO: Write tests to ensure your code works
+ * Feel free to write as many or as few tests as you feel necessary to ensure that your
+ * code is correct and stable.
+ */
+
+/*
+ * How we review this test:
+ * We look for clean, readable code, that is well designed and solves the problem.
+ * As for testing, we simply look for completeness.
+ * 
+ * Some assumptions you can make when implementing:
+ * 1) The file is always valid, you do not need to validate the file in any way
+ * 2) You may write all classes in this one file
+ */
+
+
+namespace mlp.interviews.boxing.problem
+{
+}
---
+++ SoftwareTest/SoftwareTest.cs
@@ -0,0 +1,192 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftwareTest
+{
+    /**
+     * Welcome to the Software Test. Please make sure you
+     * read the instructions carefully.
+     *
+     * FAQ:
+     * Can I use linq? Yes.
+     * Can I cheat and look things up on Stack Overflow? Yes.
+     * Can I use a database? No.
+     */
+
+    /// There are two challenges in this file
+    /// The first one should takes ~10 mins with the
+    /// second taking between ~30-40 mins.
+    public interface IChallenge
+    {
+        /// Are you a winner?
+        bool Winner();
+    }
+
+    /// Lets find out
+    public class Program
+    {
+        /// <summary>
+        /// Challenge Uno - NumberCalculator
+        ///
+        /// Fill out the TODOs with your own code and make any
+        /// other appropriate improvements to this class.
+        /// </summary>
+        public class NumberCalculator : IChallenge
+        {
+            public int FindMax(int[] numbers)
+            {
+                // TODO: Find the highest number
+                
+                int max = 0;
+                // we can also use return numbers.Max()
+                for (int i=0; i<numbers.Length; i++)
+                    if (numbers[i] > max) max = numbers[i];
+                
+                return max;
+            }
+
+            public int[] FindMax(int[] numbers, int n)
+            {
+                // TODO: Find the 'n' highest numbers
+                var sortedArray = Sort(numbers);// we can do orderby descending as well i wanted to reuse our code.
+                int len = numbers.Length;
+                if (n > len) n = len; //Incase if n is greater than array size
+                int[] maxArray = new int[n];
+                int counter = 0;
+                for (int i = len - 1; i > len-1-n; i--) { 
+                    maxArray[counter] = numbers[i];
+                    counter++;
+                }
+                return maxArray;
+            }
+
+            public int[] Sort(int[] numbers)
+            {
+                // TODO: Sort the numbers
+                return numbers.OrderBy(i => i).ToArray();
+                
+            }
+
+            public bool Winner()
+            {
+                var numbers = new[] { 5, 7, 5, 3, 6, 7, 9 };
+                var sorted = Sort(numbers); 
+                var maxes = FindMax(numbers, 2);
+                var maxes2 = FindMax(numbers, 9);
+
+                // TODO: Are the following test cases sufficient, to prove your code works
+                // as expected? If not either write more test cases and/or describe what
+                // other tests cases would be needed.
+
+                return sorted.First() == 3
+                       && sorted.Last() == 9
+                       && FindMax(numbers) == 9
+                       && maxes[0] == 9
+                       && maxes[1] == 7;
+            }
+        }
+
+        /// <summary>
+        /// Challenge Due - Run Length Encoding
+        ///
+        /// RLE is a simple compression scheme that encodes runs of data into
+        /// a single data value and a count. It's useful for data that has lots
+        /// of contiguous values (for example it was used in fax machines), but
+        /// also has lots of downsides.
+        ///
+        /// For example, aaaaaaabbbbccccddddd would be encoded as
+        ///
+        /// 7a4b4c5d
+        ///
+        /// You can find out more about RLE here...
+        /// http://en.wikipedia.org/wiki/Run-length_encoding
+        ///
+        /// In this exercise you will need to write an RLE **Encoder** which will take
+        /// a byte array and return an RLE encoded byte array.
+        /// </summary>
+        public class RunLengthEncodingChallenge : IChallenge
+        {
+            public byte[] Encode(byte[] original)
+            {
+                // TODO: Write your encoder here
+                var len = original.Length;
+                byte[] result = new byte[2 * len];
+                byte prevByte = new byte();
+                byte currentByte;
+                int counter=1;
+                int resultCounter = 0;
+                for (int i = 0;i< len; i++)
+                {
+                    currentByte = original[i];
+                    while(i+1 < len && original[i] == original[i + 1])
+                    {
+                        counter++;
+                        i++;
+                    }
+                    result[resultCounter++] = Convert.ToByte(counter);
+                    result[resultCounter++] = currentByte;
+                    counter = 1;
+                                        
+                }
+                Array.Resize<byte>(ref result, resultCounter); // resizing adds O(m) complexity as it copies reduced m size to new array
+                return result;
+            }
+
+            public bool Winner()
+            {
+                // TODO: Are the following test cases sufficient, to prove your code works
+                // as expected? If not either write more test cases and/or describe what
+                // other tests cases would be needed.
+
+                var testCases = new[]
+                {
+                    new Tuple<byte[], byte[]>(new byte[]{0x01, 0x02, 0x03, 0x04}, new byte[]{0x01, 0x01, 0x01, 0x02, 0x01, 0x03, 0x01, 0x04}),
+                    new Tuple<byte[], byte[]>(new byte[]{0x01, 0x01, 0x01, 0x01}, new byte[]{0x04, 0x01}),
+                    new Tuple<byte[], byte[]>(new byte[]{0x01, 0x01, 0x02, 0x02}, new byte[]{0x02, 0x01, 0x02, 0x02})
+                };
+
+                // TODO: What limitations does your algorithm have (if any)?
+                // TODO: What do you think about the efficiency of this algorithm for encoding data?
+                //-----------------------------------------
+                // Efficiency of this algortithm is O(m+n) where m is size of compressed array n size of original array 
+                //--------------------------------------------
+
+                foreach (var testCase in testCases)
+                {
+                    var encoded = Encode(testCase.Item1);
+                    var isCorrect = encoded.SequenceEqual(testCase.Item2);
+
+                    if (!isCorrect)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            var challenges = new IChallenge[]
+            {
+                new NumberCalculator(),
+                new RunLengthEncodingChallenge()
+            };
+
+            foreach (var challenge in challenges)
+            {
+                var challengeName = challenge.GetType().Name;
+
+                var result = challenge.Winner()
+					? string.Format("You win at challenge {0}", challengeName)
+                    : string.Format("You lose at challenge {0}", challengeName);
+
+                Console.WriteLine(result);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
